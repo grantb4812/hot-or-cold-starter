4df2bff7d90df6a70a77d0d19bf8f18488787a21
grantb4812 <grantb4812@gmail.com>
2015-08-26 17:47:27 -0500

    finished the feedback section

--- js/app.js
+++ js/app.js
@@ -39,6 +39,7 @@ var newGame = function(){
 	$("#guessList").empty();
 	guessDistance = [];
 	console.log(randomNum);
+	$('#feedback').replaceWith("<h2 id='feedback'>Make your Guess!</h2>");
 };
 
 
@@ -47,66 +48,58 @@ var guess = function(){
 	var userInput = $('#userGuess').val();
 	$('#userGuess').val("");
 	guessCount++;
-	console.log("this is first guess" + " " +userInput);
-	console.log("this is guessCount" + " " +guessCount);
 	$('#guessList').append("<li>" +userInput+ "</li>");
 	$('#userGuess').focus();
 	hotOrCold(+userInput);
 };
 
+var firstRanges = function (guess) {
+
+	if (randomNum + 5 > guess && randomNum - 5 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your really Hot!</h2>");
+	} else if (randomNum + 10 > guess && randomNum - 10 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your pretty Hot!</h2>");
+	} else if (randomNum + 20 > guess && randomNum - 20 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your cooling off!</h2>");
+	} else if (randomNum + 30 > guess && randomNum - 30 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your getting pretty cold!</h2>");
+	} else if (randomNum + 40 > guess && randomNum - 40 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your really cold!</h2>");
+	} else if (randomNum + 50 > guess && randomNum - 50 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Your probably going to freeze!</h2>");
+	} else if (randomNum + 60 > guess && randomNum - 60 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Not even close!</h2>");
+	} else if (randomNum + 70 > guess && randomNum - 70 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Is that your best guess!</h2>");
+	} else if (randomNum + 80 > guess && randomNum - 80 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>That was an awful guess!</h2>");
+	} else if (randomNum + 90 > guess && randomNum - 90 < guess) {
+		$('#feedback').replaceWith("<h2 id='feedback'>Do you even know how to count!</h2>");
+	}
+};
+
 
 var hotOrCold = function(guess){
 
-	guessDistance.push(guess);
-	distance = randomNum - guess;
-	distanceTwo = randomNum - guessDistance[0];
-
 	if (guess === randomNum) {
-		console.log("You guessed correctly!")
-	} else if (distance < distanceTwo && guessCount > 1){
-		console.log('Your going the wrong way!');
-	} else if(distance > distanceTwo && guessCount > 1){
-		console.log('this might work');
-	} else if (distance < distanceTwo && guessCount > 1){
-		console.log('Your going the wrong way!');
-	} else if (randomNum + 5 > guess && randomNum - 5 < guess) {
-		console.log('Your really Hot!');
-	} else if (randomNum + 10 > guess && randomNum - 10 < guess) {
-		console.log(('Your pretty Hot!'))
-	} else if (randomNum + 20 > guess && randomNum - 20 < guess) {
-		console.log('Your cooling off!');
-	} else if (randomNum + 30 > guess && randomNum - 30 < guess) {
-		console.log('Your getting pretty cold!')
-	} else if (randomNum + 40 > guess && randomNum - 40 < guess) {
-		console.log('Your really cold!')
-	} else if (randomNum + 50 > guess && randomNum - 50 < guess) {
-		console.log('Your probably going to freeze!')
-	} else if (randomNum + 60 > guess && randomNum - 60 < guess) {
-		console.log('Not even close!')
-	} else if (randomNum + 70 > guess && randomNum - 70 < guess) {
-		console.log('Is that your best guess!')
-	} else if (randomNum + 80 > guess && randomNum - 80 < guess) {
-		console.log('That was an awful guess!')
-	} else if (randomNum + 90 > guess && randomNum - 90 < guess) {
-		console.log('Do you even know how to count!')
+		$('#feedback').replaceWith('<h2 id="feedback">You guessed correctly!</h2>');
+	} else {
+
+		if(guessCount == 1) {
+			firstRanges(guess);
+			distanceTwo = Math.abs(randomNum - guess);
+		} else {
+
+			distance = distanceTwo;
+			distanceTwo = Math.abs(randomNum - guess);
+			if (distance > distanceTwo) {
+				$('#feedback').replaceWith('<h2 id="feedback">Your getting closer!</h2>');
+			} else {
+				$('#feedback').replaceWith('<h2 id="feedback">Your moving away!</h2>');	
+				}
+		}
 	}
+	
  
 };
 
-/*
-var secondGuess = function(guess){
-	if (guessCount <= 1){
-		var firstGuess = guess;
-	}
-
-	var firstDistance = randomNum - firstGuess;
-	if (guessCount > 1) {
-		var secondDistance = randomNum - guess;
-	}
-
-	if (secondDistance > firstDistance){
-		conole.log("Your going the wrong way");
-	}
-		
-};
-*/
